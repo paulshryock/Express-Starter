cee0012bac143d21a82e2c0c38c84c61eb9779bb
Paul Shryock <paul@pshry.com>
2019-10-17 16:07:12 -0400

    Reorder dependencies and add comments

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
-var createError = require('http-errors');
+/**
+ * Import dependencies
+ */
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var app = express();
 var { Liquid } = require('liquidjs');
 var engine = new Liquid();
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var createError = require('http-errors');
 
+/**
+ * Import routes
+ */
 var indexRouter = require('./routes/index');
 var articlesRouter = require('./routes/articles');
 var projectsRouter = require('./routes/projects');
 var testimonialsRouter = require('./routes/testimonials');
 
-var app = express();
-
-// view engine setup
+/**
+ * View engine
+ */
 app.engine('liquid', engine.express()); 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'liquid');
 
+/**
+ * Middleware
+ */
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
 
+/**
+ * Routes
+ */
 app.use('/', indexRouter);
 app.use('/api/articles', articlesRouter);
 app.use('/api/projects', projectsRouter);
 app.use('/api/testimonials', testimonialsRouter);
 
-// catch 404 and forward to error handler
+/**
+ * Catch 404 and forward to error handler
+ */
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+/**
+ * Error handler
+ */
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
