2222526a54e7371397dc08865684f7c5e0f1d7e2
Paul Shryock <paul@pshry.com>
2020-02-10 21:39:37 -0500

    Update auth middleware

--- src/server/middleware/auth.js
+++ src/server/middleware/auth.js
@@ -3,7 +3,9 @@ const config = require('config')
 
 module.exports = function (req, res, next) {
   // Get a cookie
-  // response.cookies.nameOfCookie
+  // response.cookies.x-auth-token
+
+  // Most of CSRF attacks have a different origin or referrer header with your original host in their requests. So check if you have any of them in the header, are they coming from your domain or not! If not reject them.
 
   const token = req.header('x-auth-token')
   if (!token) return res.status(401).send('Access denied. No token provided.')
