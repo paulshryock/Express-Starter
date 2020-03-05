cd30c4264de735fc026c5c4a5b3bfdd1a8a370a8
Paul Shryock <paul@pshry.com>
2020-03-05 18:14:04 -0500

    Update variables

--- src/server/app.js
+++ src/server/app.js
@@ -3,7 +3,6 @@
 /**
  * Import dependencies
  */
-require('newrelic')
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -31,6 +30,8 @@ const isProduction = app.get('env') === 'production'
 const origin = {
   origin: isProduction ? config.get('domain') : '*',
 }
+
+if (isProduction) require('newrelic') // Application monitoring
 
 /**
  * Import routes
