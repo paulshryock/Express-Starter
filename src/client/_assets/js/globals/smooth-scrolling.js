9d6b619cf9d8c1c3b649e80950dbd033045f6d2a
Paul Shryock <paul@pshry.com>
2020-03-24 18:20:34 -0400

    Fix smooth scrolling

--- src/client/_assets/js/globals/smooth-scrolling.js
+++ src/client/_assets/js/globals/smooth-scrolling.js
@@ -1,6 +1,5 @@
 /**
   * Adds hash link smooth scrolling
-  *
   */
 function addHashLinkSmoothScrolling () {
   const links = [].slice.call(document.querySelectorAll('a[href^="#"]'))
@@ -8,17 +7,17 @@ function addHashLinkSmoothScrolling () {
   links.map(link => {
     link.addEventListener('click', (e) => {
       e.preventDefault()
-
       const hash = link.getAttribute('href')
-      const target = document.querySelector(hash)
 
-      target.scrollIntoView({ behavior: 'smooth' })
+      if (hash !== '#') {
+        const target = document.querySelector(hash)
+        target.scrollIntoView({ behavior: 'smooth' })
+      }
     }, false)
   })
 }
 
 /**
   * Add hash link smooth scrolling
-  *
   */
 addHashLinkSmoothScrolling()
