ce141ac7e9ffa5d462cdc03bf7d2d69a6edb2428
Paul Shryock <paul@pshry.com>
2020-04-13 18:28:25 -0400

    Remove comment

--- src/server/app.js
+++ src/server/app.js
@@ -93,39 +93,7 @@ app.use(helmet({
   referrerPolicy: {
     policy: 'strict-origin-when-cross-origin'
   }
-})) // Set HTTP headers, along with sensible defaults
-
-// app.use(helmet.contentSecurityPolicy({
-//   directives: {
-//     defaultSrc: ["'self'"],
-//     scriptSrc: ["'self'", 'https://polyfill.io'],
-//     upgradeInsecureRequests: true
-//   }
-// })) // Set Content-Security-Policy
-// app.use(helmet.featurePolicy({
-//   features: {
-//     geolocation: ["'none'"],
-//     midi: ["'none'"],
-//     notifications: ["'none'"],
-//     push: ["'none'"],
-//     syncXhr: ["'self'"],
-//     microphone: ["'none'"],
-//     camera: ["'none'"],
-//     magnetometer: ["'none'"],
-//     gyroscope: ["'none'"],
-//     speaker: ["'none'"],
-//     vibrate: ["'none'"],
-//     fullscreen: ["'none'"],
-//     payment: ["'none'"]
-//   }
-// })) // Set Feature-Policy
-// app.use(helmet.frameguard({ action: 'deny' })) // Set X-Frame-Options
-// app.use(helmet.hsts({
-//   maxAge: 31536000, // 1 year
-//   includeSubDomains: true,
-//   preload: true
-// })) // Set Strict-Transport-Security
-// app.use(helmet.referrerPolicy({ policy: 'strict-origin-when-cross-origin' })) // Set Referrer-Policy
+})) // Set HTTP headers
 
 
 /**
